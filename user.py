7d3334376aae24faafa236a9a2373e15e32dc5d1
Александр <s79227499306@gmail.com>
2023-07-10 10:03:58 +0300

    https://github.com/MorozovA5241/rep.git

--- user.py
+++ user.py
@@ -1,4 +1,6 @@
 class User:
     main_dict: list
-    def __init__(main_dict: list):
+    user_id: int
+    def __init__(main_dict: list, user_id: int, self=None):
         self.main_dict=main_dict
+        self.user_id=user_id
